ff5069e8889488e306c41f79eb403002931103a0
Grant-Guo <xuexuguo@Xuexus-MacBook-Pro.local>
2019-04-26 16:06:21 -0400

    add test cases

--- javamodule/src/main/java/grant/guo/ideas/singleton/Application.java
+++ javamodule/src/main/java/grant/guo/ideas/singleton/Application.java
@@ -11,9 +11,15 @@ public class Application {
         int ret2 = singleton2.function();
 
         assert (ret1 == ret2);
+        assert (singleton1.equals(singleton2));
 
         MySingletonWithLazyInitialization singleton3 = MySingletonWithLazyInitialization.getInstance();
+        MySingletonWithLazyInitialization singleton4 = MySingletonWithLazyInitialization.getInstance();
+
+        assert (singleton3.equals(singleton4));
         int ret3 = singleton3.function();
         assert (ret3 == 200);
+        int ret4 = singleton4.function();
+        assert (ret3 == ret4);
     }
 }
