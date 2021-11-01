64f786f9ddcc401e7c40ee9e1a02143e6f3c8c55
grant xuexu <grant.xuexu@gmail.com>
2021-10-31 22:47:25 -0700

    updated LogParser

--- scalamodule/src/main/scala/grant/guo/ideas/log/parser/LogParser.scala
+++ scalamodule/src/main/scala/grant/guo/ideas/log/parser/LogParser.scala
@@ -1,105 +1,126 @@
 package grant.guo.ideas.log.parser
 
+import grant.guo.ideas.log.parser.Log.QUOTED
+import grant.guo.ideas.log.parser.LogParserState.{ESCAPING, KEY, KEY_START, KV_END, LogParserState, QUOTED_VALUE, START, TAILING_SPACE_IN_KEY, VALUE, VALUE_START}
+
 import scala.collection.mutable.ListBuffer
+import java.nio.charset.StandardCharsets
 
 /**
- * this class is used to parse log string which is composed of key/value pairs, separated with space into Map[String, String]
+ * this class is used to parse log string which is composed of key/value pairs, separated with space
  *
  * parser uses a state machine - LogParserStateMachine
  */
+
+case class Log(kv: List[(String, String, QUOTED)])
+object Log {
+  type QUOTED = Boolean
+}
+
 class LogParser {
-  def parse(line: String): (List[(String, String)], List[(String, Boolean)]) = {
-    val machine = new LogParserStateMachine
-    for(c <- line) {
-      machine.accept(c)
+  val stateMachine = new LogParserStateMachine
+  def parse(raw: Array[Byte]): Log = {
+    parse(new String(raw, StandardCharsets.UTF_8))
+  }
+
+  def parse(raw: String): Log = {
+    for(c <- raw) {
+      stateMachine.accept(c)
     }
-    machine.results()
+    stateMachine.done()
   }
 }
 
 class LogParserStateMachine {
-
-
-  var status = LogParserStatus.KEY // the initial state of the machine is KEY
-  val keyBuffer = ListBuffer.empty[Char]
-  val valueBuffer = ListBuffer.empty[Char]
-  val outputBuffer = ListBuffer.empty[(String, String)]
-  val fieldQuoted = ListBuffer.empty[(String, Boolean)] // if the value of a field is quoted
-
-  def accept(input: Char): Unit = {
-    status match {
-      case LogParserStatus.KEY => {
-        input match {
-          case '=' => status = LogParserStatus.KEY_END
-          case cc if(input.isDigit || input.isLetter || input.equals('_')) => keyBuffer.append(cc)
-          case _ => throw new Exception(s"invalid character in key '$input'")
-        }
-      }
-      case LogParserStatus.KEY_END => {
-        input match {
-          case '"' => status = LogParserStatus.QUOTEDVALUE
-          case _ => {
-            status = LogParserStatus.NORMALVALUE
-            valueBuffer.append(input)
-          }
-        }
-      }
-      case LogParserStatus.NORMALVALUE => {
-        input match {
-          case ' ' => {
-            status = LogParserStatus.KV_END
-            outputBuffer.append((keyBuffer.mkString(""), valueBuffer.mkString("")))
-            fieldQuoted.append((keyBuffer.mkString(""), false))
-            keyBuffer.clear()
-            valueBuffer.clear()
-          }
-          case _ => valueBuffer.append(input)
-        }
-      }
-      case LogParserStatus.QUOTEDVALUE => {
-        input match {
-          case '"' => {
-            status = LogParserStatus.KV_END
-            outputBuffer.append((keyBuffer.mkString(""), valueBuffer.mkString("")))
-            fieldQuoted.append((keyBuffer.mkString(""), true))
-            keyBuffer.clear()
-            valueBuffer.clear()
-          }
-          case '\\' => {
-            status = LogParserStatus.ESCAPING_DETECTED
-          }
-          case _ => {
-            valueBuffer.append(input)
-          }
-        }
-      }
-      case LogParserStatus.KV_END => {
-        input match {
+  var state: LogParserState = START
+  var context: Context = Context.newContext
+  def accept(c: Char): Unit = {
+    state match {
+      case START =>
+        c match {
           case ' ' =>
+          case '"' => throw new LogFormatErrorException("Double quotes can't be in KEY")
+          case _ => state = KEY_START
+        }
+      case KEY_START =>
+        c match {
+          case ' ' =>
+          case cc if (cc.isLetter || cc.equals('_')) => {
+            context.key.append(cc)
+            state = KEY
+          }
           case _ => {
-            status = LogParserStatus.KEY
-            keyBuffer.append(input)
+            println(s"${context.cache.toList}")
+            println(s"new key starts with '$c'")
+            throw new LogFormatErrorException("KEY only can start with letters or '_' ")
+          }
+        }
+      case KEY =>
+        c match {
+          case ' ' => state = TAILING_SPACE_IN_KEY
+          case '=' => state = VALUE_START
+          case _ => context.key.append(c)
+        }
+      case VALUE_START =>
+        c match {
+          case ' ' =>
+          case '"' => state = QUOTED_VALUE
+          case _ => {
+            context.value.append(c)
+            state = VALUE
+          }
+        }
+      case VALUE =>
+        c match {
+          case ' ' => {
+            context.cache.append((context.key.toString, context.value.toString, false))
+            state = KV_END
+          }
+          case _ => context.value.append(c)
+        }
+      case KV_END => {
+        context.key.setLength(0)
+        context.value.setLength(0)
+        c match {
+          case ' ' | '\t' =>
+          case _ => {
+            state = KEY_START
+            accept(c)
           }
         }
       }
-      case LogParserStatus.ESCAPING_DETECTED => {
-        status = LogParserStatus.QUOTEDVALUE
-        valueBuffer.append(input)
+      case QUOTED_VALUE =>
+        c match {
+          case '"' => {
+            context.cache.append((context.key.toString, context.value.toString, true))
+            state = KV_END
+          }
+          case '\\' => state = ESCAPING
+          case _ => context.value.append(c)
+        }
+      case ESCAPING => {
+        context.value.append('\\')
+        context.value.append(c)
+        state = QUOTED_VALUE
       }
-    }
+    }//end of state match
+  }// end of accept
+
+  def done(): Log = {
+    val ret = Log(context.cache.toList)
+    context.cache.clear()
+    ret
   }
 
-  def results(): (List[(String, String)], List[(String, Boolean)]) = {
-    (outputBuffer.toList, fieldQuoted.toList)
+  case class Context(key: StringBuffer, value: StringBuffer, cache: ListBuffer[(String, String, QUOTED)])
+  object Context {
+    def newContext: Context = Context(new StringBuffer(), new StringBuffer(), ListBuffer.empty)
   }
 }
 
-object LogParserStatus extends Enumeration {
-  type LineParseStatus = Value
-  val KEY, // processing KEY
-  KEY_END, // meet "=", which means key processing is done
-  NORMALVALUE, // value without quotes
-  QUOTEDVALUE, // value with quotes, which means there might be "\" in the value string to escape charater
-  KV_END, // done with key/value pair processing
-  ESCAPING_DETECTED = Value // a "\" detected
+object LogParserState extends Enumeration {
+  type LogParserState = Value
+  val START, KEY_START, KEY, TAILING_SPACE_IN_KEY, VALUE_START, VALUE, KV_END, QUOTED_VALUE, ESCAPING = Value
 }
+
+class LogFormatErrorException(msg: String) extends Exception(msg)
